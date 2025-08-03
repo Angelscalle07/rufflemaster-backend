747a9a8ee9cde8e13c01dac5c652eca6c0fcc4fd
Angel David Sanchez <angel.sanchez@ucp.edu.co>
2025-08-03 18:25:52 -0500

    Modificacion para poder especificar rol entre admin y paarticipante

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -15,13 +15,14 @@ class RegistroController extends Controller
             'nombre' => 'required|string|max:100',
             'email' => 'required|email|unique:usuarios,email',
             'password' => 'required|string|min:6',
+            'rol' => 'required|in:admin,participante',
         ]);
 
         $usuario = Usuario::create([
             'nombre' => $request->nombre,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'rol' => 'participante',
+            'rol' => $request->rol,
         ]);
 
         return response()->json(['mensaje' => 'Usuario registrado con éxito'], 201);
